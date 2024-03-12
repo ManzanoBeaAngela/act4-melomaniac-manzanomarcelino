60a37eec7921154417d74aa9bd9d10793349f686
ManzanoBeaAngela <manzano.beaangela@mscmarinduque.edu.ph>
2024-03-11 19:33:09 -0500

    song4

---
+++ song3.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Third Song</title>
+  <link rel="stylesheet" href="style.css"
+  </head>
+  <body>
+
+	<?php 
+echo"<div class='title'>";
+echo"<h1>Encanted</h1>";
+echo"<pre>";
+echo"\n  [Intro]";
+echo"\nG   Bm   C   G";
+ 
+echo"\n[Verse 1]";
+echo"\nG";
+echo"\nThere I was again tonight";
+echo"\nBm";
+echo"\nForcing laughter, faking smiles";
+echo"\nC                         G";
+echo"\nSame old tired, lonely place";
+echo"\nG";
+echo"\nWalls of insincerity";
+echo"\nBm";
+echo"\nShifting eyes and vacancy";
+echo"\nC                          G";
+echo"\nVanished when I saw your face";
+ 
+echo"\n                     C                   Em   D";
+echo"\nAll I can say is, it was enchanting to meet you";
+ 
+ 
+echo"\n[Verse 2]";
+echo"\nG";
+echo"\nYour eyes whispered have we met?";
+echo"\nBm";
+echo"\nCrossed the room, your silhouette";
+echo"\nC                          G";
+echo"\nStarts to make its way to me";
+echo"\n    G";
+echo"\nThe playful conversation starts";
+echo"\nBm";
+echo"\nCounter all your quick remarks";
+echo"\n    C                       G";
+echo"\nLike passing notes in secrecy";
+ 
+echo"\n       C                   Em   D";
+echo"\nAnd it was enchanting to meet you";
+echo"\n                    C                  Em   D";
+echo"\nAll I can say is, I was enchanted to meet you";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nC               G";
+echo"\n  This night is sparkling";
+echo"\nD                C";
+echo"\nDon't you let it go";
+echo"\n            G";
+echo"\nI'm wonderstruck";
+echo"\n         D             C";
+echo"\nBlushing all the way home";
+echo"\n              G    D                C";
+echo"\nI'll spend forever wondering if you knew";
+echo"\n                     Em     D  G";
+echo"\nI was enchanted to meet you-ou-ou";
+ 
+ 
+echo"\n[Verse 3]";
+echo"\n    G";
+echo"\nThe lingering question kept me up";
+echo"\nBm";
+echo"\n2 AM, who do you love?";
+echo"\n C                       G";
+echo"\nI wonder till I'm wide awake";
+echo"\n    G";
+echo"\nAnd now I'm pacing back and forth";
+echo"\nBm";
+echo"\nWishing you were at my door";
+echo"\n  C                         G";
+echo"\nI'd open up and you would say hey";
+ 
+echo"\n   C                   Em   D";
+echo"\nIt was enchanting to meet you";
+echo"\n                C                  Em   D";
+echo"\nAll I know is I was enchanted to meet you";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nC               G";
+echo"\n  This night is sparkling";
+echo"\nD                C";
+echo"\nDon't you let it go";
+echo"\n            G";
+echo"\nI'm wonderstruck";
+echo"\n         D             C";
+echo"\nBlushing all the way home";
+echo"\n              G    D                C";
+echo"\nI'll spend forever wondering if you knew";
+echo"\nC  Em  D";
+ 
+echo"\nC               G";
+echo"\n  This night is flawless";
+echo"\nD                C";
+echo"\nDon't you let it go";
+echo"\n           G";
+echo"\nI'm wonderstruck";
+echo"\n         D            C";
+echo"\nDancing around all alone";
+echo"\n              G    D                C";
+echo"\nI'll spend forever wondering if you knew";
+echo"\n                     Em   D";
+echo"\nI was enchanted to meet you";
+ 
+ 
+echo"\n[Instrumental]";
+echo"\nG   Bm   C   G";
+echo"\nG   Bm   C   D";
+echo"\n             This is me praying that";
+ 
+echo"\n[Bridge]";
+echo"\nC";
+echo"\n  This was the very first page";
+echo"\nEm              D";
+echo"\n  Not where the storyline ends";
+echo"\nC";
+echo"\n  My thoughts will echo your name";
+echo"\nEm        D";
+echo"\n  Until I see you again";
+echo"\nC                                Em        D";
+echo"\n  These are the words I held back as I was leaving too soon";
+echo"\nC                      Em   Dadd4/F#";
+echo"\n  I was enchanted to meet you";
+ 
+echo"\nG                             Bm";
+echo"\nPlease, don't be in love with someone else";
+echo"\nC                           G";
+echo"\nPlease, don't have somebody waiting on you";
+echo"\nG                             Bm";
+echo"\nPlease, don't be in love with someone else";
+echo"\nC                           G";
+echo"\nPlease, don't have somebody waiting on you";
+ 
+ 
+echo"\n[Chorus]";
+echo"\nC               G";
+echo"\n  This night is sparkling";
+echo"\nD                C";
+echo"\nDon't you let it go";
+echo"\n            G";
+echo"\nI'm wonderstruck";
+echo"\n         D             C";
+echo"\nBlushing all the way home";
+echo"\n              G    D                C";
+echo"\nI'll spend forever wondering if you knew";
+echo"\nC  Em  D";
+ 
+echo"\nC               G";
+echo"\n  This night is flawless";
+echo"\nD                C";
+echo"\nDon't you let it go";
+echo"\n            G";
+echo"\nI'm wonderstruck";
+echo"\n         D            C";
+echo"\nDancing around all alone";
+echo"\n              G    D                C";
+echo"\nI'll spend forever wondering if you knew";
+echo"\n                     Em   D";
+echo"\nI was enchanted to meet you";
+ 
+ 
+echo"\n[Outro]";
+echo"\nG                             Bm";
+echo"\nPlease, don't be in love with someone else";
+echo"\nC                           G";
+echo"\nPlease, don't have somebody waiting on you";	
+
+echo"</div>"
+ 	?>
+
+  </body>
+  </html>
