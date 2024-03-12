f0a06316cc1e5ce0658329fab3df6ee7947b3136
ManzanoBeaAngela <manzano.beaangela@mscmarinduque.edu.ph>
2024-03-11 19:31:35 -0500

    song2

---
+++ song1.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>First Song</title>
+  <link rel="stylesheet" href="style.css"
+  </head>
+  <body>
+
+  <?php 
+echo"<div class='title'>";
+echo"<h1>Night Changes</h1>";
+echo"<pre>";
+echo"\nG";
+echo"\nGoing out tonight, changes into something red";
+echo"\nEm7";
+echo"\n  Her mother doesn't like that kind of dress";
+echo"\nBm";
+echo"\nEverything she never had";
+echo"\n                 D";
+echo"\nShe’s showing off";
+echo"\nG";
+echo"\nDriving too fast, moon is breaking through her hair";
+echo"\nEm7";
+echo"\n  She's heading for something that she won’t forget";
+echo"\nBm                                              D";
+echo"\n  Having no regrets is all that she really wants";
+ 
+echo"\n[Chorus]";
+echo"\nC                    D";
+echo"\n  We're only getting older baby";
+echo"C                         D";
+echo"\n  And I've been thinking about it lately";
+echo"\nC              D";
+echo"\n  Does it ever drive you crazy";
+echo"\nG        D/F#        Em";
+echo"\nJust how fast the night changes";
+echo"\nC                        D";
+echo"\n  Everything that you've ever dreamed of";
+echo"\nC              D";
+echo"\n  Disappearing when you wake up";
+echo"\nC                        D";
+echo"\nG    D/F#        Em";
+echo"\nEven when the night changes";
+echo"\nC               D             G";
+echo"\n  It will never change me and you";
+ 
+
+echo"\n[Verse 2]";
+echo"\nG";
+echo"\nChasing her tonight, doubts are running ‘round her head";
+echo"\nEm7";
+echo"\n  He’s waiting, hides behind his cigarette";
+echo"\nBm                                                D";
+echo"\nHeart is beating loud, she doesn't want it to stop";
+echo"\nG";
+echo"\nMoving too fast, moon is lighting up her skin";
+echo"\nEm7";
+echo"\n  She’s falling, doesn't even know it yet";
+echo"\nBm                                        D";
+echo"\nHaving no regrets is all that she really wants";
+ 
+echo"\n[Chorus]";
+echo"\nC                    D";
+echo"\n  We're only getting older baby";
+echo"\nC                        D";
+echo"\n  And I've been thinking about it lately";
+echo"\nC              D";
+echo"\n  Does it ever drive you crazy";
+echo"\nG        D/F#        Em";
+echo"\nJust how fast the night changes";
+echo"\nC                        D";
+echo"\n  Everything that you've ever dreamed of";
+echo"\C              D";
+echo"\n  Disappearing when you wake up";
+echo"\nC                        D";
+echo"\n  But there’s nothing to be afraid of";
+echo"\nG    D/F#        Em";
+echo"\nEven when the night changes";
+echo"\nC               D             A";
+echo"\n  It will never change me and you";
+ 
+echo"\n[Breakdown]";
+echo"\nA  F#m  D E";
+ 
+echo"\n[Bridge]";
+echo"\nA";
+echo"\n  Going out tonight, changes into something red";
+echo"\nF#m";
+echo"\n  Her mother doesn't like that kind of dress";
+echo"\n  C#m                                            E";
+echo"\nReminds her of a missing piece of innocence she lost";
+ 
+echo"\n[Final Chorus]";
+echo"\nD                    E";
+echo"\n  We're only getting older baby";
+echo"\nD                         E";
+echo"\n  And I've been thinking about it lately";
+echo"\nD              E";
+echo"\n  Does it ever drive you crazy";
+echo"\nA        E/G#       F#m";
+echo"\nJust how fast the night changes";
+echo"\nD                        E";
+echo"\n  Everything that you've ever dreamed of";
+echo"\nD              E";
+echo"\n  Disappearing when you wake up";
+echo"\nD                         E";
+echo"\n   But there’s nothing to be afraid of";
+echo"\nA    E/G#        F#m";
+echo"\nEven when the night changes";
+echo"\nD                        E";
+echo"\n  Everything that you've ever dreamed of";
+echo"\nD              E";
+echo"\n  Disappearing when you wake up";
+echo"\nD                      E";
+echo"\nBut there’s nothing to be afraid of";
+echo"\nA    E/G#        F#m";
+echo"\nEven when the night changes";
+ 
+echo"\n[Outro]";
+echo"\nD               E";
+echo"\n  It will never change, baby";
+echo"\nD               E";
+echo"\n  We will never change, baby";
+echo"\nD               E             A";
+echo"\n  It will never change me and you";      
+
+echo"</div>"
+  ?>
+
+  </body>
+  </html>
