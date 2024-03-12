29bcf282d3e46ad8799b4964c0f14da9015ee27e
ManzanoBeaAngela <manzano.beaangela@mscmarinduque.edu.ph>
2024-03-11 19:30:58 -0500

    song1

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Melomaniac</title>
+  <style> 
+  	body {
+  		font-family: Arial; sans-serif;
+  		margin: 0;
+  		padding: 0;
+  		background-color: #ac94f4;
+  		color: #333;
+  	}
+
+ 
+.title {
+	max-width: 800px;
+	margin: 20px auto;
+	padding: 10px;
+	background-color: #fff;
+	border-radius: 10px;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.btn {
+	background-color: #643b94;
+	color: #fff;
+	border: none;
+	padding: -1px 16px;
+	border-radius: 5px;
+	text-decoration: none;
+	font-weight: bold;
+	cursor: pointer;
+	margin-bottom: 20px;
+}
+</style>
+
+  </head>
+  <body>
+
+  
+  <div class="title">
+  	<h1>Beidy Music</h1>
+  
+  	<a href="song1.php"class="btn">Night Changes by One Direction<a/><br>  	
+  	<a href="song2.php"class="btn">Yellow by Coldplay<a/><br>
+  	<a href="song3.php"class="btn">Enchanted by Taylor Swift<a/><br>
+  	<a href="song4.php"class="btn">The way I love you by Michael Leah<a/><br>
+  	<a href="song5.php"class="btn">My Love Mine All Mine by Mitski<a/><br>
+  	
+  		
+  </div>
+
+
+  </body>
+  </html>
