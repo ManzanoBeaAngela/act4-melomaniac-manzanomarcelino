0e68b45a9319ecf251c114ae73865e0ff14c8e9e
ManzanoBeaAngela <manzano.beaangela@mscmarinduque.edu.ph>
2024-03-11 19:33:52 -0500

    song5

---
+++ song4.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fourth Song</title>
+  <link rel="stylesheet" href="style.css"
+  </head>
+  <body>
+
+
+  	<?php 
+echo"<div class='title'>";
+echo"<h1>The way I love you</h1>";
+echo"<pre>";
+echo"\n[Intro]";
+echo"\nE    A  E";
+
+echo"\n [Verse 1]";
+echo"\n      E";
+echo"\nWe’ve yelled words we shouldn’t";
+echo"\n   A               E";
+echo"\nWe fought for the best";
+echo"\n      E";
+echo"\nThose tears we were crying";
+echo"\n     A         E";
+echo"\nBoth happy and sad";
+echo"\n    B                 C#m                  A";
+echo"\nOh, time it stops and people fade when you step in a room";
+echo"\nB            C#m        A        E      A";
+echo"\nI don’t love anyone the way I love you";
+ 
+echo"\nE    A  E";
+ 
+echo"\n[Verse 2]";
+echo"\n   E                         A           E";
+echo"\nWe don’t fear the silence, you read like a book";
+echo"\nE                  A            E";
+echo"\nI last a decade on one of those looks";
+echo"\nB                C#m         A";
+echo"\nI would give the ocean up to swim in your blues";
+echo"\nB            C#m        A          E";
+echo"\nI don’t love anyone the way I love you";
+echo"\n    A          E";
+echo"\nThe way I love you";
+echo"\n    A";
+echo"\nThe way I";
+ 
+echo"\n[Bridge]";
+echo"\n   D           A            E";
+echo"\nIf I know what love is it’s because of you";
+echo"\nD            A          E                B";
+echo"\nYou light my fire, yeah that’s the whole truth";
+echo"\nD           A       Am          E          F#     A";
+echo"\nI know it’s easy to get lost in all of the issues";
+ 
+echo"\n[Verse 3]";
+echo"\nE";
+echo"\nI’ll tell the whole world";
+echo"\nA         E";
+echo"\nTop of my lungs";
+echo"\nE";
+echo"\nPast, present, future";
+echo"\n     A               E";
+echo"\nThey know you’re the one";
+echo"\nB              C#m             A";
+echo"\nDoesn’t take a diamond ring to say that I do";
+echo"\nB            C#m";
+echo"\nI don’t love anyone";
+echo"\n    G#7          C#m";
+echo"\nNo, I don’t love anyone";
+echo"\nA6                      A          E";
+echo"\nI don’t love anyone the way I love you";
+ 
+echo"\n[Outro]";
+echo"\n(E)   A  E";
+echo"\n E    A  E";
+echo"\n B  C#m  A";
+echo"\n B  C#m  B  A  E";
+
+echo"</div>"
+	?>
+
+  </body>
+  </html>
